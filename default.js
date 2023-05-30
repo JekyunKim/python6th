e8026c78847bb485d04b1b8fab6b7e308fb3d853
Jekyun Kim <kkyuni97@naver.com>
2023-05-30 13:15:22 +0900

    변수

--- default.js
+++ default.js
@@ -1,28 +1,40 @@
-console.log(123, typeof 123);
-console.log(123.5, typeof 123.5);
-console.log("123", typeof "123");
-console.log(true, typeof true);
-console.log(false, typeof false);
+var name = "김제균";
+var age = 27;
+var cgpa = 3.92;
+var lineBreak = "<br/>";
 
-var car;
-console.log(car);
-var car = "";
-console.log(typeof car);
-var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
-console.log(typeof person, person);
-person = null;
+document.write("이름: " + name + lineBreak);
+document.write("나이: " + age + lineBreak);
+document.write("학점: " + cgpa + lineBreak);
 
-console.log(typeof person, person);
 
-document.write("Hello, World");
-document.write("<h1>Welcome to JS Program</h1>");
-document.write("<h2>Welcome to JS Program</h2>");
 
-console.log('Welcome JS Program');
-console.info('Welcome JS Program');
-console.warn('Welcome JS Program');
-console.error('Welcome JS Program');
 
-alert('Welcome JS Program');
-var a = prompt('Welcome JS Program');
-console.log(a);
+//console.log(123, typeof 123);
+//console.log(123.5, typeof 123.5);
+//console.log("123", typeof "123");
+//console.log(true, typeof true);
+//console.log(false, typeof false);
+//
+//var car;
+//console.log(car);
+//var car = "";
+//console.log(typeof car);
+//var person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue"};
+//console.log(typeof person, person);
+//person = null;
+//
+//console.log(typeof person, person);
+//
+//document.write("Hello, World");
+//document.write("<h1>Welcome to JS Program</h1>");
+//document.write("<h2>Welcome to JS Program</h2>");
+//
+//console.log('Welcome JS Program');
+//console.info('Welcome JS Program');
+//console.warn('Welcome JS Program');
+//console.error('Welcome JS Program');
+//
+//alert('Welcome JS Program');
+//var a = prompt('Welcome JS Program');
+//console.log(a);
