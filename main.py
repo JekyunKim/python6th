61a406b12e3fbb0a427deb1378bd11edd0fb9a60
Jekyun Kim <kkyuni97@naver.com>
2023-06-05 14:37:31 +0900

    명시적 타입 변환 float

--- main.py
+++ main.py
@@ -134,21 +134,32 @@
 # print(r)
 # print(type(r))
 
-# 명시적 타입 변환
-a = 5
-b = 2
-value = a / b
-print(value, type(value))
-int_value = int(value)
-print(int_value, type(int_value))
+# # 명시적 타입 변환
+# a = 5
+# b = 2
+# value = a / b
+# print(value, type(value))
+# int_value = int(value)
+# print(int_value, type(int_value))
+# q = 20
+# u = '10'
+# print(type(u))
+# r = q + int(u)
+# print(r, type(r))
+# r = str(q) + u
+# print(r, type(r))
+n1 = 10
+vn1 = float(n1)
+print(vn1, type(vn1))
 
-q = 20
-u = '10'
-print(type(u))
-r = q + int(u)
-print(r, type(r))
-r = str(q) + u
-print(r, type(r))
+
+
+
+
+
+
+
+
 
 
 
