2824be8e20cfa7296461758a0b2ce42891842dcd
Jekyun Kim <kkyuni97@naver.com>
2023-06-05 13:22:57 +0900

    할당 연산자

--- main.py
+++ main.py
@@ -51,22 +51,36 @@
 # print("a == b: ", a == b)
 # print("a != b: ", a != b)
 
-# 논리 연산자
-a = 5
-b = 2
-c = 3
-d = 200
+# # 논리 연산자
+# a = 5
+# b = 2
+# c = 3
+# d = 200
+#
+# print('AND 연산자')
+# print('a > b and a < c:', a > b and a < c)
+#
+# print('OR 연산자')
+# print('a > b or a < c:', a > b or a < c)
+#
+# print('NOT 연산자')
+# print('not (a < b):', not (a < b))
 
-print('AND 연산자')
-print('a > b and a < c:', a > b and a < c)
-
-print('OR 연산자')
-print('a > b or a < c:', a > b or a < c)
-
-print('NOT 연산자')
-print('not (a < b):', not (a < b))
-
-
+# # 할당 연산자 =
+# a = 10
+# b = 20
+# m = 15
+# y = a + b
+# print(y)
+#
+# m += 10
+# print(m)
+#
+# m **= 2
+# print(m)
+#
+# m //= 10
+# print(m)
 
 
 
