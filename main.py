c593698e011b784304509a8ccbcfaea3d38fbb55
Jekyun Kim <kkyuni97@naver.com>
2023-06-05 15:23:14 +0900

    출력 함수 실습

--- main.py
+++ main.py
@@ -157,12 +157,26 @@
 # print(n5, type(n5))
 # print(vn5, type(vn5))
 
-print("1")
-print("2", end='')
-print("3")
-print("4")
+# print("1")
+# print("2", end='')
+# print("3")
+# print("4")
 
+data = [10, 20, -50, 21.3, 'Likelion']
+print(data)
 
+print("Like", "Share", "Subscribe", sep='')
+print("Like", "Share", "Subscribe", sep='***')
+
+print("Like", "Share", "Subscribe", sep='***', end='\t')
+print("Like", "Share", "Subscribe", sep='***', end='\n')
+
+m = 40
+print('value: ', m)
+
+name = '김제균'
+age = 27
+print('My name is', name, 'and My age is', age)
 
 
 
