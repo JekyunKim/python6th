75782b9185175b8a8f0f0365e7136073dd98d1ef
Jekyun Kim <kkyuni97@naver.com>
2023-06-05 13:19:44 +0900

    논리 연산자

--- main.py
+++ main.py
@@ -42,17 +42,29 @@
 # # 몫(음수)
 # print("몫(음수) = -a // b: ", -a // b)
 
-# 비교 연산자
+# # 비교 연산자
+# a = 5
+# b = 2
+# print("a: ", a, "b: ", b)
+# print("a < b: ", a < b)
+# print("a <= b: ", a <= b)
+# print("a == b: ", a == b)
+# print("a != b: ", a != b)
+
+# 논리 연산자
 a = 5
 b = 2
-print("a: ", a, "b: ", b)
-print("a < b: ", a < b)
-print("a <= b: ", a <= b)
-print("a == b: ", a == b)
-print("a != b: ", a != b)
+c = 3
+d = 200
 
+print('AND 연산자')
+print('a > b and a < c:', a > b and a < c)
 
+print('OR 연산자')
+print('a > b or a < c:', a > b or a < c)
 
+print('NOT 연산자')
+print('not (a < b):', not (a < b))
 
 
 
