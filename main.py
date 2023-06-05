3168643a151ffbf6a7b3bf9ffc4aadf65b5048d1
Jekyun Kim <kkyuni97@naver.com>
2023-06-05 13:11:01 +0900

    비교 연산자 실습

--- main.py
+++ main.py
@@ -1,17 +1,79 @@
-# 변수 선언
-a = 10
-b = 3.14
-c = "Hello, World!"
-d = [1, 2, 3]
-e = (4, 5, 6)
-f = {7, 8, 9}
-g = {"apple": 1, "banana": 2, "orange": 3}
+# # 변수 선언
+# a = 10
+# b = 3.14
+# c = "Hello, World!"
+# d = [1, 2, 3]
+# e = (4, 5, 6)
+# f = {7, 8, 9}
+# g = {"apple": 1, "banana": 2, "orange": 3}
+#
+# # 데이터 타입 출력
+# print("a: ", type(a))
+# print("b: ",type(b))
+# print("c: ",type(c))
+# print("d: ",type(d))
+# print("e: ",type(e))
+# print("f: ",type(f))
+# print("g: ",type(g))
 
-# 데이터 타입 출력
-print("a: ", type(a))
-print("b: ",type(b))
-print("c: ",type(c))
-print("d: ",type(d))
-print("e: ",type(e))
-print("f: ",type(f))
-print("g: ",type(g))
+# # 연산자
+# # 덧셈
+# a = 4
+# b = 2
+# total = a + b
+# print("덧셈 = a + b: ", total)
+# # 뺄셈
+# c = 4
+# d = 2
+# total = c - d
+# print("뺄셈 = c - d: ", total)
+# # 곱셈
+# print("곱셈 = a * b: ",  a * b, type(a * b))
+# # 나눗셈
+# print("나눗셈 = a / b: ", a / b, type(a / b))
+# # 나머지 연산
+# e = 5
+# f = 2
+# print("나머지 = e % f: ", e % f)
+# # 거듭제곱
+# print("거듭제곱 = a ** b: ", a ** b)
+# # 몫(양수)
+# print("몫(양수) = a // b: ", a // b)
+# # 몫(음수)
+# print("몫(음수) = -a // b: ", -a // b)
+
+# 비교 연산자
+a = 5
+b = 2
+print("a: ", a, "b: ", b)
+print("a < b: ", a < b)
+print("a <= b: ", a <= b)
+print("a == b: ", a == b)
+print("a != b: ", a != b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
