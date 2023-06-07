57bfe1cb839b62a7c5cd4880a5d65641e51dae49
Jekyun Kim <kkyuni97@naver.com>
2023-06-07 15:28:29 +0900

    중첩 함수 실습

--- 230607.py
+++ 230607.py
@@ -1,39 +1,8 @@
-def add():
-    x = 10
-    y = 20
-    c = x + y
-    return c
+def disp():
+    def show():
+        return "Show Function"
+    result = show() + "Disp Function"
+    return result
 
 
-sum1 = add()
-print(sum1)
-
-def add2():
-    x = 10
-    y = 20
-    return x + y
-
-
-sum2 = add2()
-print(sum2)
-
-def add3(y):
-    x = 10
-    return x + y
-
-
-sum3 = add3(20)
-print(sum3)
-
-def add4(y):
-    x = 10
-    c = x + y
-    d = y - x
-    return c, d, 50
-
-
-sum4, sub1, a = add4(20)
-print(sum4)
-print(sub1)
-print(a)
-print(type(add4(20)))
+print(disp())
