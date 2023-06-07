2c89598ffa8c9da05ead799d0c4028480f6e248f
Jekyun Kim <kkyuni97@naver.com>
2023-06-07 09:32:17 +0900

    if문 실습

---
+++ 230607.py
@@ -0,0 +1,61 @@
+if else
+age = 18
+if age >= 18:
+    print('성인입니다.')
+else:
+    print('미성년자입니다.')
+
+중첩된 if else
+score = 55
+if score >= 90:
+    print('A')
+else:
+    if score >= 80:
+        print('B')
+    else:
+        if score >= 70:
+            print('C')
+        else:
+            print('D')
+
+elif
+marks = 75
+if marks >= 90:
+    print('A')
+elif marks >= 80:
+    print('B')
+elif marks >= 70:
+    print('C')
+else:
+    print('D')
+
+a = int(input("Enter Number Greater than or Equal to 2:"))
+if a >= 2:
+    print("Correct!! You have Entered: ", a)
+else:
+    print("Wrong!! You have Entered: ", a)
+
+day = input("요일을 입력하세요: ")
+if day == "Mon":
+    print("오늘은 월요일")
+elif day == "Tue":
+    print("오늘은 화요일")
+elif day == "Wed":
+    print("오늘은 수요일")
+else:
+    print("휴일")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
