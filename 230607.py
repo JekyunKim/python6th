52423bfd3ae7a0ee781af1e9b2a6e3771741d23c
Jekyun Kim <kkyuni97@naver.com>
2023-06-07 15:12:49 +0900

    함수 실습, f문자열

--- 230607.py
+++ 230607.py
@@ -1,9 +1,28 @@
-s = "Hello,World"
+print("parameter 없는 함수")
 
-print(s.replace("World", "there"))
-print(s.replace("world".title(), "Jekyun"))
-split_str = s.split(",")
-print(split_str)
-print(" ".join(split_str))
-print(s.startswith("Hello"))
-print(s.endswith("World"))
+def disp():
+    name = "멋쟁이 사자"
+    print("Welcome to", name)
+
+print("함수 실행")
+disp()
+
+def add(y):
+    x = 10
+    c = x + y
+    print(c)
+
+add(20)
+
+def add(y):
+    x = 10.2334
+    print(x + y)
+    print(f"Formatted Output{x + y:5.2f}")
+    print(f"Formatted Output{x + y:f}")
+    print(f"Formatted Output{x + y:.2f}")
+    print(f"Formatted Output{x + y:10f}")
+
+add(20)
+
+
+
