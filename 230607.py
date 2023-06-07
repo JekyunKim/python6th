f759ed95b59cd759526edfbeb0d634fb2b1747a8
Jekyun Kim <kkyuni97@naver.com>
2023-06-07 14:32:48 +0900

    문자열 함수

--- 230607.py
+++ 230607.py
@@ -1,14 +1,9 @@
-s = " Hello World "
+s = "Hello,World"
 
-print(s.upper())
-print(s.lower())
-print(s.swapcase())
-print(s.title())
-print(s.isupper())
-print(s.islower())
-print(s.istitle())
-print(s.isdigit())
-print(s.isalpha())
-print(s.lstrip())
-print(s.rstrip())
-print(s.strip())
+print(s.replace("World", "there"))
+print(s.replace("world".title(), "Jekyun"))
+split_str = s.split(",")
+print(split_str)
+print(" ".join(split_str))
+print(s.startswith("Hello"))
+print(s.endswith("World"))
