ea9aa6c2bc9ca73ba24e5740cee6da3a4dc19615
Jekyun Kim <kkyuni97@naver.com>
2023-06-08 10:15:01 +0900

    위치 인수, 키워드 인수 실습

--- 230607.py
+++ 230607.py
@@ -1,9 +1,15 @@
-def disp():
-    def show():
-        return "Show Function"
-    print("Disp Function")
-    return show
+def pw(x, y):
+    z = x ** y
+    print(z)
 
 
-r_sh = disp()
-print(r_sh(), type(r_sh))
+pw(2, 5)
+pw(5, 2)
+
+def show(name, age):
+    print(f"Name: {name} Age: {age}")
+
+
+show(name="멋쟁이사자", age=42)
+show(age=42, name="멋쟁이사자")
+
