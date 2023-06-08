9166f76c8b3710c2e4b481a1eee5ea05e28c017f
Jekyun Kim <kkyuni97@naver.com>
2023-06-08 15:39:04 +0900

    재귀호출

--- 230608.py
+++ 230608.py
@@ -1,10 +1,14 @@
-def val(x):
-    print("Inside: ", x, id(x))
-    x += 1
-    print("Inside After: ", x, id(x))
+import sys
+print("default: ", sys.getrecursionlimit())
+sys.setrecursionlimit(100)
+print("setting: ", sys.getrecursionlimit())
 
-print("======")
-x = 10
-print("Before Calling: ", x, id(x))
-val(x)
-print("After Calling: ", x, id(x))
+i = 0
+def myfun():
+    global i
+    i += 1
+    print("My Function: ", i)
+    myfun()
+
+
+myfun()
