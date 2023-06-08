a6566595812047197fe58778a18ebc5f30b939dc
Jekyun Kim <kkyuni97@naver.com>
2023-06-08 14:01:46 +0900

    튜플

--- 230607.py
+++ 230607.py
@@ -1,12 +1,54 @@
-def generate_alphabet(start_letter, end_letter):
-    start = ord(start_letter)
-    end = ord(end_letter)
-    while start <= end:
-        yield chr(start)
-        start += 1
+b = (10)
+c = (10,)
+print(type(b))
+print(type(c))
 
+d = (10, 20, 30, 40)
+e = (10, 20, -50, 21.3, '멋쟁이사자',)
+f = 10, 20, -50, 21.3, '멋쟁이사자'
 
-runner = generate_alphabet('A', 'F')
+print(d, e, f, sep="\n")
 
-for letter in runner:
-    print(letter)
+print(f[1])
+print(f[2])
+print(f[3])
+print(f[4])
+print(f[:3])
+print(f[1:4])
+print(f[3:])
+print(c + f)
+print(f * 3)
+print(-10 in f)
+print("========")
+h = (10, 20, -50, 20)
+print(min(h), max(h))
+print(h.count(20))
+print(h.index(20))
+
+sorted_a = sorted(h)
+print((sorted_a))
+
+a = (10, 20, -50)  # Unpacking
+x, y, z = a
+print(x, y, z)
+
+a = 1
+b = 2
+print(a, b)
+
+tmp = a  # 전통적인 변수 바꿔치기
+a = b
+b = tmp
+print(a, b)
+
+a, b = b, a
+print(a, b)
+
+list_h = list(h)
+print(list_h, type(list_h))
+
+tuple_h = tuple(list_h)
+print(tuple_h, type(tuple_h))
+
+nested_tuple = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
+print(nested_tuple)
