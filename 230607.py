4df19731a8f7f1e75fa1ae65a9236d355a137349
Jekyun Kim <kkyuni97@naver.com>
2023-06-08 14:11:56 +0900

    기본 Set 실습

--- 230607.py
+++ 230607.py
@@ -1,54 +1,19 @@
-b = (10)
-c = (10,)
+a = {10, 20, 30}
+a = {10, 20, 30, '멋쟁이사자', 'Kim', 40}
+a = {10, 20, 30, '멋쟁이사자', 'Kim', 40, 10, 20}
+
+b = set()
 print(type(b))
-print(type(c))
+a.add(50)
+a.update([10, 20, 60, 70])
+print(a)
+a.remove('멋쟁이사자')   # 지워줘 했는데 없으면 error
+a.discard('멋쟁이사자')  # 지워줘 했는데 없으면 그냥 지나감
+a.discard(70)
+print(a)
 
-d = (10, 20, 30, 40)
-e = (10, 20, -50, 21.3, '멋쟁이사자',)
-f = 10, 20, -50, 21.3, '멋쟁이사자'
+new_set = a.copy()
+new_set.clear()
+print(new_set)
 
-print(d, e, f, sep="\n")
-
-print(f[1])
-print(f[2])
-print(f[3])
-print(f[4])
-print(f[:3])
-print(f[1:4])
-print(f[3:])
-print(c + f)
-print(f * 3)
-print(-10 in f)
-print("========")
-h = (10, 20, -50, 20)
-print(min(h), max(h))
-print(h.count(20))
-print(h.index(20))
-
-sorted_a = sorted(h)
-print((sorted_a))
-
-a = (10, 20, -50)  # Unpacking
-x, y, z = a
-print(x, y, z)
-
-a = 1
-b = 2
-print(a, b)
-
-tmp = a  # 전통적인 변수 바꿔치기
-a = b
-b = tmp
-print(a, b)
-
-a, b = b, a
-print(a, b)
-
-list_h = list(h)
-print(list_h, type(list_h))
-
-tuple_h = tuple(list_h)
-print(tuple_h, type(tuple_h))
-
-nested_tuple = ((1, 2, 3), (4, 5, 6), (7, 8, 9))
-print(nested_tuple)
+# print(a[0]) set은 인덱스로 접근 불가, 순서X
