198745c21bdfca9b06302558ef19cf5b9427e38d
Jekyun Kim <kkyuni97@naver.com>
2023-06-12 13:05:00 +0900

    패키지 실습

--- 230612.py
+++ 230612.py
@@ -1,23 +1,5 @@
-class Engine:
-    def start(self):
-        return "Engine started"
+from MyApp.Handlers.text_handler import handle_text
 
-    def stop(self):
-        return "Engine stopped"
+input_txt = "python package practice"
+handle_text(input_txt)
 
-
-class Wheels:
-    def rotate(self):
-        return "Wheels are rotating"
-
-
-# 다중 상속
-class Car(Engine, Wheels):
-    pass
-
-
-# 인스턴스 생성
-my_car = Car()
-# 부모 클래스의 메소드 사용
-print(my_car.start())  # 출력: "Engine started"
-print(my_car.rotate())  # 출력: "Wheels are rotating"
