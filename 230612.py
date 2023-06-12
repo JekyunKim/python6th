113b91bba3f0a1b557635bb4cd6f30a307d77c47
Jekyun Kim <kkyuni97@naver.com>
2023-06-12 10:20:26 +0900

    클래스 실습

---
+++ 230612.py
@@ -0,0 +1,25 @@
+class Car:
+    # 클래스 속성
+    wheels = 4
+
+    def __init__(self, make, model, color):
+        self.make = make
+        self.model = model
+        self.color = color
+
+    # Method
+    def drive(self):
+        return "The car is moving!"
+
+    def stop(self):
+        return "The car has stopped"
+
+
+my_car = Car("KIA", "Morning", "Blue")
+
+# 속성 사용
+print(my_car.make)
+
+# 메소드 호출
+print(my_car.drive())
+print(my_car.stop())
